ccf6d0a9e5a6fa09ac30edca58b1e75540fbe7ea
Zander <alexander.backlin.salas@iths.se>
2024-12-08 16:35:21 +0100

    Tutorial

--- examples/app.js
+++ examples/app.js
@@ -48,7 +48,7 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           // Fetches a random emoji to send from a helper function
-          content: `hello world ${getRandomEmoji()}`,
+          content: `Arte melle! ${getRandomEmoji()}`,
         },
       });
     }
